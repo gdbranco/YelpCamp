21d756e27518cde6b726262ebb53856a6cb88dff
dbranco <gdbranco@gmail.com>
2017-07-16 01:54:49 +0000

    Fixing password required

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 var userSchema = mongoose.Schema({
         username: {type: String, required: true, unique: true},
-        password: {type: String, required: true},
+        password: {type: String},
         first: String,
         last: String,
         email: {type: String, unique: true},
